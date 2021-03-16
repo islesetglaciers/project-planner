048111723d8fd67e5e71689536c260fc5dc423b2
sidd sirius <sidd.benoit@gmail.com>
2021-03-16 11:01:01 -0400

    filterNav

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,45 @@
 <template>
   <div class="home">
-    Home
+    <FilterNav @filterChange="current = $event" :current="current" />
+    <div v-if="projects.length">
+      <div v-for="project in projects" :key="project.id">
+        <SingleProject :project="project" @delete="handleDelete" @complete="handleComplete" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import SingleProject from '../components/SingleProject.vue'
+import FilterNav from '../components/FilterNav.vue'
+
 export default {
   name: 'Home',
-  components: { }
+  components: { SingleProject, FilterNav },
+  data() {
+    return {
+      projects: [],
+      current: 'all'
+    }
+  },
+  mounted() {
+    fetch('http://localhost:3000/projects')
+      .then(res => res.json())
+      .then(data => this.projects = data)
+      .catch(err => console.log(err.message))
+  },
+  methods: {
+    handleDelete(id) {
+      this.projects = this.projects.filter((project) => {
+        return project.id !== id
+      })
+    },
+    handleComplete(id) {
+      let proj = this.projects.find(project => {
+        return project.id === id
+      })
+      proj.complete = !proj.complete
+    }
+  }
 }
 </script>
