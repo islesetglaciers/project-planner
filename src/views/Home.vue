2169562c16c4294787b3717af0aff295aa6264fb
sidd sirius <sidd.benoit@gmail.com>
2021-03-26 11:04:22 -0400

    i really need to use fb

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -23,7 +23,7 @@ export default {
     }
   },
   mounted() {
-    fetch('../db.json')
+    fetch('http://localhost:3000/projects')
       .then(res => res.json())
       .then(data => this.projects = data)
       .catch(err => console.log(err.message))
