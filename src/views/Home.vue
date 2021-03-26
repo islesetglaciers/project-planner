cecd5cc98b93120094e97c1e91c9f8190a7ccb16
sidd sirius <sidd.benoit@gmail.com>
2021-03-26 10:50:24 -0400

    fix fix fix that test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -23,7 +23,7 @@ export default {
     }
   },
   mounted() {
-    fetch('http://localhost:3000/projects')
+    fetch('https://eloquent-albattani-e3bdd6.netlify.app/projects')
       .then(res => res.json())
       .then(data => this.projects = data)
       .catch(err => console.log(err.message))
